57e5de081e5836ee28b1533792635ef91c57c055
Annan Deriba <annanderiba@Annans-MacBook-Air.local>
2024-05-31 13:27:49 +0300

    Added comments

--- 24_Passive_Buzzer_Mini_Alarm_Project/24_Passive_Buzzer_Mini_Alarm_Project.ino
+++ 24_Passive_Buzzer_Mini_Alarm_Project/24_Passive_Buzzer_Mini_Alarm_Project.ino
@@ -14,6 +14,7 @@ void setup() {
 
   Serial.begin(9600);
 
+  // Start by asking the user to set a password
   Serial.println(message1);
   while (Serial.available() == 0){
 
@@ -28,17 +29,17 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (Serial.available() != 0){
+  if (Serial.available() != 0){ // As soon as the user enters something on the serial monitor, we'll get into this if block
     user_input = Serial.readString();
 
-    if (user_input == password){
+    if (user_input == password){ // Check if what they typed is the same as the password set earlier. 
       Serial.println("Entered password is correct!");
       Serial.println("Alarm switched off");
       Serial.println("Type something to turn it back on.");
       Serial.println();
 
-      // Switching the alarm off the correctly enter the password  
-      while (Serial.available() == 0){
+      // Switching the alarm off  
+      while (Serial.available() == 0){//We're using a while loop here because as soon as they send something we want to get out of it and start the buzzing
         digitalWrite(buzzer_pin, LOW);
         delayMicroseconds(delay_time);
         digitalWrite(buzzer_pin, LOW);
@@ -52,6 +53,7 @@ void loop() {
     }
   }
 
+  // If there is nothing sent through the serial monitor then the alarm will buzz annoyingly :)
   digitalWrite(buzzer_pin, HIGH);
   delayMicroseconds(delay_time);
   digitalWrite(buzzer_pin, LOW);
